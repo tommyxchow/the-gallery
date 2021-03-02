bc32a52025342ab292003e84436307ee09ba4797
Tommy Chow <tommycho@buffalo.edu>
2021-03-02 03:28:29 -0500

    test

---
+++ functions.js
@@ -0,0 +1,13 @@
+function welcomeAlert(){
+	alert("If you're seeing this, your server sent functions.js!")
+	console.log("Test")
+}
+
+function loadImage(id){
+	document.getElementsByName
+	var imageName = document.getElementById(id).innerHTML
+
+	var test = document.getElementById("display")
+	test.src = "/image/" + id + ".jpg"
+
+}
---
+++ functions.py
@@ -0,0 +1,44 @@
+def getRequestPath(requestString):
+    end = requestString.rfind(' ')
+    return requestString[4: end]
+
+#encode string into a byte array and ship it
+def buildResponse200(mimeType, length, content):
+    httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\nX-Content-Type-Options: nosniff\r\n\r\n{}".format(mimeType, length, content)
+
+    return httpResponse.encode()
+
+def buildResponse301(path):
+    httpResponse = "HTTP/1.1 301 Moved Permanently\r\nLocation: {}\r\nX-Content-Type-Options: nosniff\r\n\r\n".format(path)
+
+    return httpResponse.encode()
+
+def buildResponse404(mimeType, errorMessage):
+    httpResponse = "HTTP/1.1 404 Not Found\r\nContent-Type: {}\r\nContent-Length: {}\r\nX-Content-Type-Options: nosniff\r\n\r\n{}\r\n\r\n".format(mimeType, len(errorMessage), errorMessage)
+
+    return httpResponse.encode()
+
+def buildResponseBinary(mimeType, bArray):
+    httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\nX-Content-Type-Options: nosniff\r\n\r\n".format(mimeType, len(bArray))
+
+    httpResponse = httpResponse.encode() + bArray
+
+    return httpResponse
+
+def queryToDictionary(path):
+    if path.find("=") != -1:
+        kv = {}
+        split = path[path.find("?")+1:].split("&")
+        for pair in split:
+            splitEquals = pair.split("=")
+            for keyValue in splitEquals:
+                splitPlus = keyValue.split("+")
+                kv[splitEquals[0]] = splitPlus
+
+        return kv
+
+
+
+test = "/images?images=cat+kitten+dog&name=Mitch"
+
+print(handleQuery(test))
